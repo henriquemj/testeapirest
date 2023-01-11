074a76cd4a728dc1100103d3cd2965df320213d1
henriquemj <henriquemadureira05@gmail.com>
2023-01-11 08:26:15 -0300

    adicionando metodos de pesquisa

--- src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
+++ src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
@@ -11,6 +11,8 @@ import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Arrays;
+
 
 public class UserJsonTest {
 	
@@ -86,6 +88,42 @@ public class UserJsonTest {
 			.body("error", is("Usuário inexistente"))
 			;
 	}
-
+	
+	@Test
+	public void deveVerificarListaRaiz() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", hasSize(3))
+			.body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+			.body("age[1]", is(25))
+			.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+			.body("salary", contains(1234.5678f, 2500, null))
+			;
+	}
+	
+	@Test
+	public void devoFazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", hasSize(3))
+			.body("age.findAll{it <= 25}.size()", is(2))
+			.body("age.findAll{it <= 25 && it > 20}.size()", is((1)))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+			.body("find{it.age <= 25}.name", is("Maria Joaquina"))
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+//			.body("name.findAll{it.startWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			;
+		
+	}
 
 }
