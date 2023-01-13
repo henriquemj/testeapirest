490bfcae54562f08ea0247c7e3c1418b5cb241df
henriquemj <henriquemadureira05@gmail.com>
2023-01-13 19:20:39 -0300

    metodo fazendo verificação avançadas

--- src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
+++ src/main/java/br/ce/wcaquino/rest/UserJsonTest.java
@@ -121,7 +121,13 @@ public class UserJsonTest {
 			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
 			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
 			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
-//			.body("name.findAll{it.startWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+			.body("age.collect{it * 2}", hasItems(60, 50, 40))
+			.body("id.max()", is(3))
+			.body("salary.min()", is(1234.5678f))
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(5000d)))
 			;
 		
 	}
