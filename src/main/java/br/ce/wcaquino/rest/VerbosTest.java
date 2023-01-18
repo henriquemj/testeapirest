3b5e739bdeabd4e67798c582ae07c935291c56ba
henriquemj <henriquemadureira05@gmail.com>
2023-01-18 09:20:01 -0300

    Serializando objeto

--- src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -52,6 +52,26 @@ public class VerbosTest {
 	}
 	
 	@Test
+	public void deveSalvarUsuarioUsandoObjeto() {
+		
+		User user = new User("Usuario via objeto", 35);
+		
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuario via objeto"))
+			.body("age", is(35))
+		;
+	}
+	
+	@Test
 	public void naoDeveSalvarUsuarioSemNome() {
 		given()
 			.log().all()
