1a73a488c41f9b87c3f8acdeea75d49c78191814
henriquemj <henriquemadureira05@gmail.com>
2023-01-18 19:42:52 -0300

    Deserializando objeto

--- src/main/java/br/ce/wcaquino/rest/User.java
+++ src/main/java/br/ce/wcaquino/rest/User.java
@@ -2,6 +2,7 @@ package br.ce.wcaquino.rest;
 
 public class User {
 	
+	private Long id;
 	private String name;
 	private Integer age;
 	private Double salary;
@@ -13,6 +14,14 @@ public class User {
 		this.age = age;
 	}
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -31,4 +40,12 @@ public class User {
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + "]";
+	}
+	
+	
+	
 }
--- src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -6,6 +6,7 @@ import static org.hamcrest.Matchers.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
@@ -69,6 +70,29 @@ public class VerbosTest {
 			.body("name", is("Usuario via objeto"))
 			.body("age", is(35))
 		;
+	}
+	
+	@Test
+	public void deveDeserializarObjetoAoSalvarUsuario() {
+		
+		User user = new User("Usuario deserializado", 35);
+		
+		User usuarioInserido = given()
+			.log().all()
+			.contentType("application/json")
+			.body(user)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.extract().body().as(User.class)
+		;
+		
+		System.out.println(usuarioInserido);
+		Assert.assertThat(usuarioInserido.getId(), notNullValue());
+		Assert.assertEquals("Usuario deserializado", usuarioInserido.getName());
+		Assert.assertThat(usuarioInserido.getAge(), is(35));
 	}
 	
 	@Test
