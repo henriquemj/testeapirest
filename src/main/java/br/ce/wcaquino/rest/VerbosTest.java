624dfbf2c0afda9fea88dfe697c02305ab10a685
henriquemj <henriquemadureira05@gmail.com>
2023-01-18 09:07:12 -0300

    Serializando MAP

--- src/main/java/br/ce/wcaquino/rest/VerbosTest.java
+++ src/main/java/br/ce/wcaquino/rest/VerbosTest.java
@@ -2,6 +2,10 @@ package br.ce.wcaquino.rest;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import io.restassured.http.ContentType;
@@ -23,6 +27,27 @@ public class VerbosTest {
 			.body("id", is(notNullValue()))
 			.body("name", is("Jose"))
 			.body("age", is(50))
+		;
+	}
+	
+	@Test
+	public void deveSalvarUsuarioUsandoMap() {
+		Map<String, Object> params = new HashMap<String, Object>();
+		params.put("name", "Usuario via map");
+		params.put("age", 25);
+		
+		given()
+			.log().all()
+			.contentType("application/json")
+			.body(params)
+		.when()
+			.post("https://restapi.wcaquino.me/users")
+		.then()
+			.log().all()
+			.statusCode(201)
+			.body("id", is(notNullValue()))
+			.body("name", is("Usuario via map"))
+			.body("age", is(25))
 		;
 	}
 	
