af373cc05f41eef49b0bd06c2c49d8f68b49ca15
henriquemj <henriquemadureira05@gmail.com>
2023-01-18 19:52:56 -0300

    Serializado em XML

--- src/main/java/br/ce/wcaquino/rest/User.java
+++ src/main/java/br/ce/wcaquino/rest/User.java
@@ -1,19 +1,28 @@
 package br.ce.wcaquino.rest;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
+
 public class User {
-	
+
 	private Long id;
 	private String name;
 	private Integer age;
 	private Double salary;
-	
-	
+
+	public User() {
+	}
+
 	public User(String name, Integer age) {
 		super();
 		this.name = name;
 		this.age = age;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
@@ -25,18 +34,23 @@ public class User {
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Integer getAge() {
 		return age;
 	}
+
 	public void setAge(Integer age) {
 		this.age = age;
 	}
+
 	public Double getSalary() {
 		return salary;
 	}
+
 	public void setSalary(Double salary) {
 		this.salary = salary;
 	}
@@ -45,7 +59,5 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", age=" + age + ", salary=" + salary + "]";
 	}
-	
-	
-	
+
 }
