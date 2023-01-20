e1d4d97bdbcfa99c2c73a541e5c2f0fc985631a5
henriquemj <henriquemadureira05@gmail.com>
2023-01-20 19:58:38 -0300

    Upload de documento 200 / 404

---
+++ src/main/java/br/ce/wcaquino/rest/FileTest.java
@@ -0,0 +1,40 @@
+package br.ce.wcaquino.rest;
+
+import static org.hamcrest.Matchers.is;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+
+import org.junit.Test;
+
+public class FileTest {
+	
+	@Test
+	public void deveObrigarEnvioArquivo() {
+		given()
+			.log().all()
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.statusCode(404)
+			.body("error", is("Arquivo não enviado"))
+			;
+	}
+	
+	@Test
+	public void devoFazerUploadArquivo() {
+		given()
+			.log().all()
+			.multiPart("arquivo", new File("src/main/resources/users.pdf"))
+		.when()
+			.post("http://restapi.wcaquino.me/upload")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("name", is("users.pdf"))
+			;
+	}
+
+}
